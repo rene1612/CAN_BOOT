2925e6d8330b8224b78f325da0bff197ff20ca39
Rene Schönrock <rene.schoenrock@online.de>
2024-12-04 21:44:32 +0100

    git handling angepasst (softwarehandling)

---
+++ Core/Inc/gitcommit.h
@@ -0,0 +1,5 @@
+#define GIT_INFO_PRESENT
+#define __GIT_SHORT_HASH__ 0x1b59388
+#define __GIT_BRANCH__ "HEAD -> dev, github/dev"
+#define __GIT_DATE_STR__ "2024-11-25"
+#define __GIT_DATE_UT__ 1732571820
